a78bd7ac9377bb4cf2b5695be31a20a427d3214d
forrestrose <46428067+forrestrose@users.noreply.github.com>
2024-08-25 20:03:36 -0700

    Update custom_text_header.js

--- css/addons/unraid/login-page/retro-terminal/js/custom_text_header.js
+++ css/addons/unraid/login-page/retro-terminal/js/custom_text_header.js
@@ -1,18 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Custom Text Header //
+    // ACSII slant font
 
- //Custom Text Header //
- // ACSII slant font
-
-// Needs to be inside <section id="login" class="shadow">
-// YOU MUST ESCAPE ANY SINGLE BACKSLASHES LIKE SO: \\
-
-let custom_text_header = `
-<div class="custom-text-header"> <pre>                     __                             ___       __  
+    let custom_text_header = `
+    <div class="custom-text-header">
+        <pre>                     
+                     __                             ___       __  
   ______              _____ __                  
  /_  __/_______  ___ / ___// /_  ____ _________ 
-  / / / ___/ _ \/ _ \\__ \/ __ \/ __ `/ ___/ _ \
+  / / / ___/ _ \\/ _ \\\\__ \\/ __ \\/ __ \`/ ___/ _ \\
  / / / /  /  __/  __/__/ / / / / /_/ / /  /  __/
-/_/ /_/   \___/\___/____/_/ /_/\__,_/_/   \___/ 
+/_/ /_/   \\___/\\___/____/_/ /_/\\__,_/_/   \\___/ 
                                                 
-                                                                 </pre> </div>
-`;
-document.getElementById("login").innerHTML += custom_text_header
+        </pre>
+    </div>
+    `;
+    document.getElementById("login").innerHTML += custom_text_header;
+});
